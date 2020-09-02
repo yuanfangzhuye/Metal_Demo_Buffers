3a54f4519ab065c06b7bcf9b65152a85607ab3bb
chao.li <chao.li@team-lab.cn>
2020-09-02 14:36:09 +0800

    Initial Commit

---
+++ Metal_Demo_Buffers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Metal_Demo_Buffers
+//
+//  Created by tlab on 2020/9/2.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
